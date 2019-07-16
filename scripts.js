52cebddb6174309e1ba0b3574d59b3cc1f41d68a
Cat <blackcatstone@gmail.com>
2019-07-16 10:52:07 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function showAbout() {
-    var x = document.getElementById("aboutBox");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
