84d1455f8266cc78ce29341bf5770372014e5f61
Cat <blackcatstone@gmail.com>
2019-08-24 16:39:01 +0100

    Add expanding behaviour

---
+++ main.js
@@ -0,0 +1,11 @@
+var portfolioItems = document.querySelectorAll( '.portfolio-item' )
+
+portfolioItems.forEach( function( item ) {
+    item.setAttribute( 'data-state', 'closed' )
+    item.addEventListener( 'click', function() {
+        item.setAttribute( 'data-state', item.getAttribute( 'data-state') === 'open' ? 'closed' : 'open' )
+    })
+    console.log( item )
+})
+
+portfolioItems[0].setAttribute( 'data-state', 'open' )
