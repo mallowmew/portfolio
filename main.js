0e8eb7486ebe96acb4f60690668ec7cef2d81411
Cat <blackcatstone@gmail.com>
2019-08-24 16:53:14 +0100

    Fix indentation in main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 var portfolioItems = document.querySelectorAll( '.portfolio-item' )
 
 portfolioItems.forEach( function( item ) {
-    item.setAttribute( 'data-state', 'closed' )
-    item.addEventListener( 'click', function() {
-        portfolioItems.forEach( function( innerItem ) {
-            innerItem.setAttribute( 'data-state', 'closed' )
-        })
-        item.setAttribute( 'data-state', item.getAttribute( 'data-state') === 'open' ? 'closed' : 'open' )
+  item.setAttribute( 'data-state', 'closed' )
+  item.addEventListener( 'click', function() {
+    portfolioItems.forEach( function( innerItem ) {
+      innerItem.setAttribute( 'data-state', 'closed' )
     })
-    console.log( item )
+    item.setAttribute( 'data-state', item.getAttribute( 'data-state') === 'open' ? 'closed' : 'open' )
+  })
+  console.log( item )
 })
 
 portfolioItems[0].setAttribute( 'data-state', 'open' )
